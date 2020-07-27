599efd900c74db1c8ed83305935b12b847e77204
Joe <57957983+joseph-cavarretta@users.noreply.github.com>
2020-07-26 18:32:06 -0600

    Update ex07.py

--- ex07.py
+++ ex07.py
@@ -1,13 +1,10 @@
-# prints a string
+# Exercise 7 "More printing"
+
 print ("Mary had a little lamb.")
-#prints a string with a format bracket to change the word in the string
 print ("Its fleece was white as {}.".format('snow'))
-#prints a string
 print ("And everywhere that Mary went.")
-#prints a string x 10
 print ("." * 10)
 
-#define multiple variables as strings
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -21,6 +18,5 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-#prints the variables with a space to make both lines on the same line
 print (end1 + end2 + end3 + end4 + end5 + end6, end = " ")
 print (end7 + end8 + end9 + end10 + end11 + end12)
