3030c1f5412db9fce1963c68dbad3ddd073e7e71
Joe <57957983+joseph-cavarretta@users.noreply.github.com>
2020-07-26 18:11:43 -0600

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,16 @@
+print ("I will now count my chickens")
+print ("Hens", 25.0 + 30.5 / 6.7)
+print ("roosters", 100 - 25 * 3 % 4)
+print ("Now i will count the eggs")
+print (3 + 2 + 1 - 5 + 5 % 2 - 1 / 4 + 6)
+print ("is it true that 3 + 2 < 5 - 7?")
+print (3+2 < 5-7)
+print ("what is 3 + 2?", 3+2)
+print ("what is 5 - 7?", 5-7)
+print ("oh thats why it was false")
+
+print ("how about some more.")
+
+print ("is it greater?", 5>-2)
+print ("is it greater or equal to?", 5>= -2)
+print ("is it less or equal to?", 5<= -2)
